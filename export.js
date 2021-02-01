92d36045a4e1f0b3c4faac64d59686f59311ce76
capn-21 <chalkeom99@gmail.com>
2021-02-01 11:45:21 +0530

    2

---
+++ export.js
@@ -0,0 +1,12 @@
+function math(){
+    var a=64,b=34,c;
+    c=a+b;
+    console.log(c)
+    
+}
+
+module.exports=math;
+
+exports.datefun=function(){
+    return Date();
+};
---
+++ import.js
@@ -0,0 +1,3 @@
+const math =require('./export.js')
+
+console.log(math())
