fd59c9d1c97019565d5b113fc97c24a286fe37a2
capn-21 <chalkeom99@gmail.com>
2021-03-12 23:14:51 +0530

    initial commit

--- logger.js
+++ logger.js
@@ -1,4 +1,14 @@
-function lets(add)
-{
-    console.log(add);
+function lets()
+{var a=44,b=12,sum;
+    sum=a+b;
+    console.log(sum)
+    
 }
+
+class sdone{
+constructor(){
+    console.log("constructor created");
+}
+}
+
+module.exports=test;
