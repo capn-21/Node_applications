58c040db460fc47d6db0cbb01b0d3d83c4f91f79
capn-21 <chalkeom99@gmail.com>
2021-05-21 23:24:50 +0530

    1

---
+++ circle.js
@@ -0,0 +1,20 @@
+function area(r){
+var ans;
+   ans = (3.14*r*r);
+
+  
+    console.log(ans);
+}
+
+//area(3);
+
+function circ(r){
+    var c;
+       c = 2*3.14*r;
+    
+      
+        console.log(c);
+    }
+    
+
+    circ(3);
---
+++ factorial.js
@@ -0,0 +1,16 @@
+function fact(){
+var n=4;
+    var f=1;
+
+    for(var int=1;int<=n;int++){
+
+        f=f*int;
+
+      
+    }
+    console.log(f);
+    
+
+}
+
+fact();
